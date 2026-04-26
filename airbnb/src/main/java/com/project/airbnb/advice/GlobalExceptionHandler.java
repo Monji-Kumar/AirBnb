d01878f43134c174ae2934ae16785784468b5b6d
MANOJ <workwithmonji@gmail.com>
2026-04-26 16:04:07 +0530

    Global Response Handlers

--- airbnb/src/main/java/com/project/airbnb/advice/GlobalExceptionHandler.java
+++ airbnb/src/main/java/com/project/airbnb/advice/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.project.airbnb.advice;
 
+import com.project.airbnb.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception exception) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(error);
+    }
+
+    private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError error) {
+        return new ResponseEntity<>(new ApiResponse<>(error), error.getStatus());
+    }
 }
